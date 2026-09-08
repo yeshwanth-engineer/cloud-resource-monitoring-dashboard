0910a60eca4a8c68acabe9cc68b7bf04ba5676a0
Yeshwanth G <yeshwanth1017@gmail.com>
2026-09-08 20:13:46 +0530

    feat: add Spring Boot application

---
+++ backend/src/main/java/com/yeshwanth/cloudmonitor/CloudMonitorApplication.java
@@ -0,0 +1,11 @@
+package com.yeshwanth.cloudmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudMonitorApplication.class, args);
+    }
+}

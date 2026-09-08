30bc369e5605439dcc4276010ec22a35155ab24e
Yeshwanth G <yeshwanth1017@gmail.com>
2026-09-08 20:15:04 +0530

    fix: import JWT authentication exception

--- backend/src/main/java/com/yeshwanth/cloudmonitor/controller/AuthController.java
+++ backend/src/main/java/com/yeshwanth/cloudmonitor/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.yeshwanth.cloudmonitor.controller;
 import com.yeshwanth.cloudmonitor.security.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
